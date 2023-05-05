98e90877a410d4dccc750c270c60fb69debd8cd8
Jack Watters <johnwatt@usc.edu>
2023-05-04 19:18:05 -0700

    Skeleton and scripts set up and working.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,4 +2,11 @@ import express from "express";
 
 const router = express.Router();
 
+/* GET home page. */
+router.get("/", (_req, res) => {
+	res.render("index", { title: "Express" });
+});
+
+// change..
+
 export default router;
